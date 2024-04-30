701b0d0f6b8700ac2b2c5d4faa81546496d55da1
ahpoise <36297960+ahpoise@users.noreply.github.com>
2024-04-30 18:31:20 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# React State
+
+1. Address the issues in the file Counter.jsx to make the counter work
+2. Refactor the code to had a decrement button. Don't forget to check if state is equal to 0 as we don't want negative values
---
+++ src/Counter.jsx
@@ -0,0 +1,18 @@
+// import useState
+
+export default function Counter() {
+  // use state hook
+
+  // create a funtion to increment the count, increment by 1
+
+  return (
+    <div>
+      {/* use state to make the integer dynamic */}
+      <p>The count is: {0}</p>
+      <div>
+        {/* update the onClick with your function */}
+        <button onClick={() => {}}>Increment</button>
+      </div>
+    </div>
+  );
+}
