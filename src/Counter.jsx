e16f88a19514db4be2f1175f5168b7d36a7fe892
Rafael Guerra <rafa99gm@hotmail.com>
2024-05-01 09:33:42 +0100

    done

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,15 +1,24 @@
 // import useState
+import { useState } from "react";
 
 export default function Counter() {
   // use state hook
+  const [num, setNum] = useState(0);
+
+  const changeNum = (int) => {
+    if(typeof int !== 'number' || num + int < 0) return;
+    setNum(num + int);
+  }
+
 
   return (
     <div>
       {/* use state to make the integer dynamic */}
-      <p>The count is: {0}</p>
+      <p>The count is: {num}</p>
       <div>
         {/* update the onClick with your function */}
-        <button onClick={() => {}}>Increment</button>
+        <button onClick={() => {changeNum(1)}}>Increment</button>
+        <button onClick={() => {changeNum(-1)}}>Decrement</button>
       </div>
     </div>
   );
